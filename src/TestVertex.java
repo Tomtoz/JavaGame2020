5a26fc5024d81cab1645b91237501de58e662ed1
TomZoeller <TomZoeller@0-208-09>
2020-12-08 15:37:07 +0100

    TestVertex VertextJava

---
+++ src/TestVertex.java
@@ -0,0 +1,18 @@
+
+public class TestVertex {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Vertex v1 = new Vertex(42,4);
+		Vertex v2 = new Vertex(23,12);
+		
+		System.out.println(v1);
+		
+	
+		
+		System.out.println(v2);
+		System.out.println(v1.length());
+		
+	}
+
+}
